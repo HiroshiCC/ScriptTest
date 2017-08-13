ec1540acf214aaedb8797ffa0103d10abc57ee77
高須　寛 <h-takasu@mwa.biglobe.ne.jp>
2017-08-13 15:34:56 +0900

    課題の提出（初版）
    配列の値を、順番に表示／逆順に表示

---
+++ Assets/Test.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Test : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        int i;
+
+        // 5個の箱を用意
+        int[] Value = new int[5];
+
+        //値をセット
+        Value[0] = 10;
+        Value[1] = 5;
+        Value[2] = 1;
+        Value[3] = 3;
+        Value[4] = 8;
+
+        // 順番に表示
+        Debug.Log("順番に表示");
+        for (i = 0; i < Value.Length; i++)
+        {
+            Debug.Log(Value[i]);
+        }
+
+        // 逆順に表示
+        Debug.Log("逆順に表示");
+        for (i = Value.Length-1; i>=0 ; i--)
+        {
+            Debug.Log(Value[i]);
+        }
+
+    }
+
+    // Update is called once per frame
+    void Update () {
+		
+	}
+}
