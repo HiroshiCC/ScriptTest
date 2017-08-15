dabbc238e53139cfb3a7f64e2bcfcfde90d7247e
高須　寛 <h-takasu@mwa.biglobe.ne.jp>
2017-08-15 14:31:29 +0900

    発展課題に対応
    Bossクラスの新規追加。魔法処理を追加。

--- Assets/Test.cs
+++ Assets/Test.cs
@@ -2,10 +2,51 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Test : MonoBehaviour {
+public class Boss
+{
+    int mp = 53;        // マジックパワー
+    int mpCost = 5;     // 魔法１回に消費するmp
+    int count = 0;      // 魔法トライ回数
 
-	// Use this for initialization
-	void Start () {
+    public void Attack()
+    {
+        // 攻撃用の関数
+    }
+
+    public void Defence(int damage)
+    {
+        // 防御用の関数
+    }
+
+    // ***************************************
+    //  魔法を使う
+    //  戻り値  1:魔法成功   0:魔法失敗
+    // ***************************************
+    public int Magic()
+    {
+        count++;
+        Debug.Log( count + "回目");
+
+        // 魔法用の関数
+        if (mp < mpCost) {
+            Debug.Log("MPが足りないため魔法が使えない。");
+            return 0;
+        }
+        else
+        {
+            mp -= mpCost;
+            Debug.Log("魔法攻撃をした。残りMPは" + mp + "。");
+            return 1;
+        }
+    }
+
+}
+
+
+public class Test : MonoBehaviour {
+    // Use this for initialization
+    void Start() {
+#if BASIC
         int i;
 
         // 5個の箱を用意
@@ -31,8 +72,22 @@ public class Test : MonoBehaviour {
         {
             Debug.Log(Value[i]);
         }
+#else
+        int result = 0; // 魔法結果
+        Boss lastBoss = new Boss();
 
-    }
+        while (true) { 
+            result = lastBoss.Magic();
+            if (result == 0)
+            {
+                // 魔法失敗
+                Debug.Log("mpが足りない！");
+                break;
+            }
+        };
+#endif
+
+}
 
     // Update is called once per frame
     void Update () {
